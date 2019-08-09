2d826f5eefe208eefc73d23cda9d60231253a6e4
Котов Александр Петрович <kotov.a.petr@sberbank.ru>
2019-08-09 23:10:14 +0300

    Add cloud-api module

---
+++ cloud-api/src/main/kotlin/com/alexandrkotov/cloudSpaceManager/cloudapi/Application.kt
@@ -0,0 +1,11 @@
+package com.alexandrkotov.cloudSpaceManager.cloudapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ cloud-api/src/test/kotlin/com/alexandrkotov/cloudSpaceManager/cloudapi/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.alexandrkotov.cloudSpaceManager.cloudapi
+
+import org.junit.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
