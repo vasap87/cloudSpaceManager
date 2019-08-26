954622bcd6a7e1e96a4d7e96ec46f697eb125563
Alexandr Kotov <a200x@bk.ru>
2019-08-26 12:23:07 +0300

    fix user-api

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-subprojects {
-	group = "com.alexandrkotov.cloudspacemanager"
-	version = "0.0.1"
-
-	repositories {
-		jcenter()
-		mavenCentral()
-	}
-}
--- user-api/src/main/kotlin/com/alexandrkotov/cloudSpaceManager/userapi/controller/RegistrationController.kt
+++ user-api/src/main/kotlin/com/alexandrkotov/cloudSpaceManager/userapi/controller/RegistrationController.kt
@@ -2,8 +2,6 @@ package com.alexandrkotov.cloudSpaceManager.userapi.controller
 
 import com.alexandrkotov.cloudSpaceManager.userapi.model.WebUser
 import com.alexandrkotov.cloudSpaceManager.userapi.services.UserService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,10 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class RegistrationController {
-
-    @Autowired(required = false)
-    lateinit var userService: UserService
+class RegistrationController(private val userService: UserService) {
 
     @PostMapping("/registration")
     fun registration(@RequestBody user: WebUser): ResponseEntity<String> {
